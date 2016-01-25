c3f810c5157cb9790e88125a6ac4d0376d9a4fdc
mahmood321 <reshadtarafdar@gmail.com>
2016-01-25 14:39:46 -0500

    Finished exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#q1. In order to calculate a tip, lets say 15%, we would multipy 0.15 into 55.
+puts 55 * 0.15
+
+#q2. In order to convert the integer into a string, we put .to_s
+puts "My favorite number is" + 25.to_s
+
+#q3.
+puts "45628 multiplied by 7839 is #{45628*7839}."
+
+#q4.
+(10 < 20 && 30 < 20) || !(10 == 11)
